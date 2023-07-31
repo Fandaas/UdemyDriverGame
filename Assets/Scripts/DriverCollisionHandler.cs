4391cf1270c0d2e6e92835d6cc9c79d960fdc880
Fandaas <mozisfrantisek@gmail.com>
2023-07-31 23:21:24 +0200

    6th challenge -triggers

--- Assets/Scripts/DriverCollisionHandler.cs
+++ Assets/Scripts/DriverCollisionHandler.cs
@@ -7,5 +7,10 @@ public class DriverCollisionHandler : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log($"DriverCollisionHandler >> Kolize s {collision.gameObject.name}");
-    } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"DriverCollisionHandler >> Trigger s {collision.gameObject.name}");
+    }
 }
